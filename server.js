aa5b3186ab1bd763835e4b1dcda98cfe8c3cbc5c
whisky87 <vanbinh87@gmail.com>
2018-05-29 16:16:59 +0700

    Organize the structure of source code: Seperate config files for each process_type (api, worker, etc.)

--- server.js
+++ server.js
@@ -2,26 +2,21 @@
 
 //save the root_path of application
 global.root_path = __dirname;
+var config = require('./lib/config');
 
 var http = require('http');
 var path = require('path');
 var debug = require('debug')('server');
 var shopRouter = require('./lib/api/routes');
-var env = require('dotenv').config({ path: './.env' });
+
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var utils = require('./lib/utils');
-//DB Mongo
-var db = require('./lib/config/db');
-db.connect();
 
-if (env.error) {
 
-    throw new Error('Error loading .env file: ' + env.error.message);
-}
 
-var port = process.env.SB_API_PORT || 3001;
+var port = process.env.PORT || 3001;
 
 var myPath = path.join(root_path, 'vendor', 'contract.js')
 debug(myPath);
